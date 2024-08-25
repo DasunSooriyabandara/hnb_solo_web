3be64ed85052e0a665c8a9f4cdb1db81fa8b87a3
Dasun Sooriyabandara <115138972+DasunSooriyabandara@users.noreply.github.com>
2024-08-26 01:13:42 +0530

    fill tform continuous

--- src/test/java/Objects/AddOutletPage.java
+++ src/test/java/Objects/AddOutletPage.java
@@ -8,9 +8,15 @@ import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.google.common.base.Function;
+
 import java.time.Duration;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Random;
 
 public class AddOutletPage {
 
@@ -27,9 +33,13 @@ public class AddOutletPage {
 	private By merchantType = By.xpath("//*[@id=\"form-outlet-merchant-type-field\"]/div/div[2]/div");
 	private By merchantTypeOpt = By.id("react-select-8-option-0");
 	private By merchantName = By.xpath("//*[@id=\"outlet-merchant-id-field\"]/div/div[1]");
-	private By merchantNameOpt = By.id("react-select-15-option-0");
-	private By UploadImageBtn = By.xpath("//*[@id=\"app\"]/div[2]/div/div/div[2]/div/form/div[1]/div[17]/div/div/div/div[1]/label/span");
-
+	private By merchantNameOpt = By.id("react-select-15-option-3");
+	private By UploadImageBtn = By.xpath("//span[contains(text(),'Select an Image')]");
+	private By MID = By.id("outlet-merchant-transaction-type-id-field");
+	private By SAVE = By.xpath("//*[@id=\"app\"]/div[2]/div/div/div[2]/div/form/div[3]/button[2]");
+	private FluentWait<WebDriver> wait111;
+	private FluentWait<WebDriver> wait11;
+	
 	// Method to navigate to the 'My Outlets' page and click the 'Add' button
 	public void clickAddButton() {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -43,7 +53,7 @@ public class AddOutletPage {
 
 		// ---------------Enter the name-------------------
 		WebElement enterNameElement = wait.until(ExpectedConditions.visibilityOfElementLocated(enterName));
-		enterNameElement.sendKeys("Test Name");
+		enterNameElement.sendKeys("Automation Test");
 
 		// ----------------Click on the Merchant Type dropdown----------------
 		WebElement merchantTypeElement = wait.until(ExpectedConditions.elementToBeClickable(merchantType));
@@ -74,40 +84,75 @@ public class AddOutletPage {
 
 		System.out.println("Option selected successfully: " + selectedOption.getText());
 
-		// ----------------Click on the Merchant Name dropdown----------------
+		
+		
+		
+		
+//		// ----------------Click on the Merchant Name dropdown----------------
 		WebElement merchanNameElement = wait.until(ExpectedConditions.elementToBeClickable(merchantName));
 		merchanNameElement.click();
-
-		// Wait for the options to be visible
+//
+//		// Wait for the options to be visible
 		WebElement merchanNameElement1 = wait.until(ExpectedConditions.visibilityOfElementLocated(merchantNameOpt));
-
-		// Scroll the option into view
+//
+//		// Scroll the option into view
 		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", merchanNameElement1);
-
-		// Remove potential overlays
+//
+//		// Remove potential overlays
 		((JavascriptExecutor) driver)
 				.executeScript("var elements = document.getElementsByClassName('PrivateSwipeArea-root-227');"
 						+ "while(elements.length > 0) { elements[0].parentNode.removeChild(elements[0]); }");
-
-		// Click on the option
+//
+//		// Click on the option
 		merchanNameElement1.click();
-
+		
 		Thread.sleep(3000);
-		// ------------Verify the option was
-		// selected------------------------------------------
-		WebElement selectedOption1 = wait.until(ExpectedConditions.visibilityOfElementLocated(merchantName));
+		wait11 = null;
+//		// ------------Verify the option was
+//		// selected------------------------------------------
+		WebElement selectedOption1 = wait11.until(ExpectedConditions.visibilityOfElementLocated(merchantName));
 		selectedOption1.getText().contains("Expected Option Text");
 		selectedOption1.click();
-
+//
 		System.out.println("Option selected successfully: " + selectedOption.getText());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		Thread.sleep(3000);
 //		---------------------------------------------------------------------------------------------------------------
 
-		WebElement selectImage = wait.until(ExpectedConditions.visibilityOfElementLocated(UploadImageBtn));
+		 wait111 = null;
+		WebElement fileInputElement = wait111.until(ExpectedConditions.visibilityOfElementLocated(UploadImageBtn));
+	     fileInputElement.sendKeys("\"C:\\Users\\USER\\Downloads\\signature.png\"");  // Replace with the correct file path
+
+	     System.out.println("Image uploaded successfully.");
 		
-		selectImage.sendKeys("https://drive.google.com/file/d/1Flur__1ZUsg3LLR2ouE3gVqGr6hq0CYi/view?usp=drive_link");
-		 
-		
-		
+//		-------------------------------------------------------------------------------------------------------------------
+	     Thread.sleep(3000);
+	     WebElement enterMID = wait111.until(ExpectedConditions.visibilityOfElementLocated(MID));
+	     enterMID.sendKeys("12345678");
+	
+	     System.out.println("Enter MID successfully.");
+	     
+	     
+	     WebElement ClickSaveBtn = wait111.until(ExpectedConditions.elementToBeClickable(SAVE));
+	     ClickSaveBtn.click();
+	     System.out.println("Save Add Outlet Details Successfully");
+
 	}
 }
